c64458d6cf5507d1f9fc184fae2338adb2fbb510
leemo10 <leemo10@outlook.com>
2016-09-13 19:59:36 +0100

    committing changes

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,7 @@
+import com.pluralsight.AppConfig;
 import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Created by leemorris on 25/08/2016.
@@ -8,9 +10,14 @@ import com.pluralsight.service.CustomerServiceImpl;
     public class Application {
 
         public static void main(String[] args){
-            CustomerService service = new CustomerServiceImpl();
+
+            ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+            //CustomerService service = new CustomerServiceImpl();
+
+            CustomerService service = appContext.getBean("customerService", CustomerService.class);
 
             System.out.println(service.findAll().get(0).getFirstName());
         }
     }
+
 
